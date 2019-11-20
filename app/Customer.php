fa75d13ca69aee57f8d64075b051ea3f2e000fbf
cravindr <cravindr@gmail.com>
2019-11-20 17:04:51 +0530

    20/11/19 completed

--- app/Customer.php
+++ app/Customer.php
@@ -5,12 +5,14 @@ namespace App;
 use DB;*/
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 
 /*class Customer extends Eloquent*/
 class Customer extends Model
 {
     protected $table = 'customer';
+    protected $primaryKey='id';
     //
 
     public function getPlace()
@@ -32,4 +34,27 @@ class Customer extends Model
     {
         return $this->hasOne('App\Place','id','place');
     }
+
+    public static function getMaxCode($id)
+    {
+        $ret= DB::select(DB::raw("select concat(p.short_code,
+                                                   LPAD(
+                                                       if(
+                                                           Max(
+                                                                    convert(
+                                                                             mid(code, 4, length(code))
+                                                                            , UNSIGNED
+                                                                            )
+                                                              ) is null,1,Max(convert(mid(code, 4, length(code)), UNSIGNED))+1
+                                                           ),
+                                                       6,0))
+                                             as Code from customer
+                                            join place p on customer.place = p.id
+                                            where place=$id"));
+
+        $rets=$ret[0];
+        return $rets->Code;
+    }
+
+
 }
